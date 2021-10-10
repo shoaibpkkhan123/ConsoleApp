1ab86300794186a0eff440065a54a4431c28b5cf
shoaibpkkhan123 <89197091+shoaibpkkhan123@users.noreply.github.com>
2021-10-10 20:57:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var first_name="Shoaib";
+var second_name="khan";
+var a = 5;
+var b = 10;
+var add = a+b;	 
+console.log("Sum of Two number is "+ add);
+concat();
+
+function concat()
+{
+    var fullname = first_name + " " + second_name; 
+    console.log(fullname);
+}
+
+
+
